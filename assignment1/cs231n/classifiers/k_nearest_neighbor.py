1c5b56005a9002724f1ec998b23de7b18a7b050f
Marcin Przewięźlikowski <m.przewie@gmail.com>
2018-02-07 10:51:49 +0100

    additional cleanups in assignment1

--- assignment1/cs231n/classifiers/k_nearest_neighbor.py
+++ assignment1/cs231n/classifiers/k_nearest_neighbor.py
@@ -3,13 +3,13 @@ from past.builtins import xrange
 
 
 class KNearestNeighbor(object):
-  """ a kNN classifier with L2 distance """
+    """ a kNN classifier with L2 distance """
 
-  def __init__(self):
-    pass
+    def __init__(self):
+        pass
 
-  def train(self, X, y):
-    """
+    def train(self, X, y):
+        """
     Train the classifier. For k-nearest neighbors this is just 
     memorizing the training data.
 
@@ -19,11 +19,11 @@ class KNearestNeighbor(object):
     - y: A numpy array of shape (N,) containing the training labels, where
          y[i] is the label for X[i].
     """
-    self.X_train = X
-    self.y_train = y
-    
-  def predict(self, X, k=1, num_loops=0):
-    """
+        self.X_train = X
+        self.y_train = y
+
+    def predict(self, X, k=1, num_loops=0):
+        """
     Predict labels for test data using this classifier.
 
     Inputs:
@@ -37,19 +37,19 @@ class KNearestNeighbor(object):
     - y: A numpy array of shape (num_test,) containing predicted labels for the
       test data, where y[i] is the predicted label for the test point X[i].  
     """
-    if num_loops == 0:
-      dists = self.compute_distances_no_loops(X)
-    elif num_loops == 1:
-      dists = self.compute_distances_one_loop(X)
-    elif num_loops == 2:
-      dists = self.compute_distances_two_loops(X)
-    else:
-      raise ValueError('Invalid value %d for num_loops' % num_loops)
+        if num_loops == 0:
+            dists = self.compute_distances_no_loops(X)
+        elif num_loops == 1:
+            dists = self.compute_distances_one_loop(X)
+        elif num_loops == 2:
+            dists = self.compute_distances_two_loops(X)
+        else:
+            raise ValueError('Invalid value %d for num_loops' % num_loops)
 
-    return self.predict_labels(dists, k=k)
+        return self.predict_labels(dists, k=k)
 
-  def compute_distances_two_loops(self, X):
-    """
+    def compute_distances_two_loops(self, X):
+        """
     Compute the distance between each test point in X and each training point
     in self.X_train using a nested loop over both the training data and the 
     test data.
@@ -62,43 +62,43 @@ class KNearestNeighbor(object):
       is the Euclidean distance between the ith test point and the jth training
       point.
     """
-    num_test = X.shape[0]
-    num_train = self.X_train.shape[0]
-    dists = np.zeros((num_test, num_train))
-    for i in xrange(num_test):
-      for j in xrange(num_train):
-        dists[i][j] = ((self.X_train[j] - X[i]) ** 2).sum()
-    return dists
+        num_test = X.shape[0]
+        num_train = self.X_train.shape[0]
+        dists = np.zeros((num_test, num_train))
+        for i in xrange(num_test):
+            for j in xrange(num_train):
+                dists[i][j] = ((self.X_train[j] - X[i]) ** 2).sum()
+        return dists
 
-  def compute_distances_one_loop(self, X):
-    """
+    def compute_distances_one_loop(self, X):
+        """
     Compute the distance between each test point in X and each training point
     in self.X_train using a single loop over the test data.
 
     Input / Output: Same as compute_distances_two_loops
     """
-    num_test = X.shape[0]
-    num_train = self.X_train.shape[0]
-    dists = np.zeros((num_test, num_train))
-    for i in xrange(num_test):
-        dists[i] = ((X[i] - self.X_train) ** 2).sum(axis=1)
-    return dists
+        num_test = X.shape[0]
+        num_train = self.X_train.shape[0]
+        dists = np.zeros((num_test, num_train))
+        for i in xrange(num_test):
+            dists[i] = ((X[i] - self.X_train) ** 2).sum(axis=1)
+        return dists
 
-  def compute_distances_no_loops(self, X):
-    """
+    def compute_distances_no_loops(self, X):
+        """
     Compute the distance between each test point in X and each training point
     in self.X_train using no explicit loops.
 
     Input / Output: Same as compute_distances_two_loops
     """
-    num_test = X.shape[0]
-    num_train = self.X_train.shape[0]
-    # dists = np.zeros((num_test, num_train)) 
-    dists = (((X **2)).sum(axis=1) - 2 * (self.X_train @ X.T)).T  + ((self.X_train **2)).sum(axis=1)
-    return dists
+        num_test = X.shape[0]
+        num_train = self.X_train.shape[0]
+        # dists = np.zeros((num_test, num_train))
+        dists = (((X ** 2)).sum(axis=1) - 2 * (self.X_train @ X.T)).T + ((self.X_train ** 2)).sum(axis=1)
+        return dists
 
-  def predict_labels(self, dists, k=1):
-    """
+    def predict_labels(self, dists, k=1):
+        """
     Given a matrix of distances between test points and training points,
     predict a label for each test point.
 
@@ -110,16 +110,15 @@ class KNearestNeighbor(object):
     - y: A numpy array of shape (num_test,) containing predicted labels for the
       test data, where y[i] is the predicted label for the test point X[i].  
     """
-    num_test = dists.shape[0]
-    y_pred = np.zeros(num_test)
-    for i in xrange(num_test):
-        # A list of length k storing the labels of the k nearest neighbors to
-        # the ith test point.
-        idx = np.argsort(dists[i])
-        closest_y = [self.y_train[j] for j in idx[:k]]
-        counts = np.bincount(closest_y)
-        
-        y_pred[i] = np.argmax(counts)
+        num_test = dists.shape[0]
+        y_pred = np.zeros(num_test)
+        for i in xrange(num_test):
+            # A list of length k storing the labels of the k nearest neighbors to
+            # the ith test point.
+            idx = np.argsort(dists[i])
+            closest_y = [self.y_train[j] for j in idx[:k]]
+            counts = np.bincount(closest_y)
 
-    return y_pred
+            y_pred[i] = np.argmax(counts)
 
+        return y_pred
--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -2,8 +2,9 @@ import numpy as np
 from random import shuffle
 from past.builtins import xrange
 
+
 def softmax_loss_naive(W, X, y, reg):
-  """
+    """
   Softmax loss function, naive implementation (with loops)
 
   Inputs have dimension D, there are C classes, and we operate on minibatches
@@ -20,75 +21,73 @@ def softmax_loss_naive(W, X, y, reg):
   - loss as single float
   - gradient with respect to weights W; an array of same shape as W
   """
-  # Initialize the loss and gradient to zero.
-  loss = 0.0
-  dW = np.zeros_like(W)
+    # Initialize the loss and gradient to zero.
+    loss = 0.0
+    dW = np.zeros_like(W)
 
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using explicit loops.     #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-  for i, x in enumerate(X):
-    h = x @ W
-    good_xe = np.e ** h[y[i]]
-    rest_xe = np.sum(np.e ** h)
-    loss_tmp = -np.log(good_xe / rest_xe)
-    loss += loss_tmp
-     
-    grad_tmp = x  / rest_xe
-    for j in range(W.shape[1]):
-        dW[:, j] += grad_tmp * (np.e ** h[j])
-    dW[:, y[i]] -= x
-    
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
-  loss /= X.shape[0] 
-  loss += reg * np.sum(W **2)
-  dW /= X.shape[0] 
-  dW += reg * 2 * W
-  return loss, dW
+    #############################################################################
+    # TODO: Compute the softmax loss and its gradient using explicit loops.     #
+    # Store the loss in loss and the gradient in dW. If you are not careful     #
+    # here, it is easy to run into numeric instability. Don't forget the        #
+    # regularization!                                                           #
+    #############################################################################
+    for i, x in enumerate(X):
+        h = x @ W
+        good_xe = np.e ** h[y[i]]
+        rest_xe = np.sum(np.e ** h)
+        loss_tmp = -np.log(good_xe / rest_xe)
+        loss += loss_tmp
+
+        grad_tmp = x / rest_xe
+        for j in range(W.shape[1]):
+            dW[:, j] += grad_tmp * (np.e ** h[j])
+        dW[:, y[i]] -= x
+
+    #############################################################################
+    #                          END OF YOUR CODE                                 #
+    #############################################################################
+    loss /= X.shape[0]
+    loss += reg * np.sum(W ** 2)
+    dW /= X.shape[0]
+    dW += reg * 2 * W
+    return loss, dW
 
 
 def softmax_loss_vectorized(W, X, y, reg):
-  """
+    """
   Softmax loss function, vectorized version.
 
   Inputs and outputs are the same as softmax_loss_naive.
   """
-  # Initialize the loss and gradient to zero.
-  #print('kierwa')
-  loss = 0.0
-  dW = np.zeros_like(W)
+    # Initialize the loss and gradient to zero.
+    loss = 0.0
+    dW = np.zeros_like(W)
 
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-  H = X @ W
-  XE = np.e ** H
-  true_indices = np.arange(y.shape[0]), y
-  good_XE = XE[true_indices]
-  XE_sums = XE.sum(axis=1)
-  loss = (-np.log(good_XE / XE_sums)).sum() / X.shape[0] + reg * np.sum(W **2)
-  
-  grad_tmp = (X.T / XE_sums)
-  #print(grad_tmp.shape, XE.shape)
-  dW += (grad_tmp @ XE)
-  for i in range(W.shape[1]):
-    dW[:, i] -= X[np.argwhere(y == i)].sum(axis=0).squeeze()
+    #############################################################################
+    # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
+    # Store the loss in loss and the gradient in dW. If you are not careful     #
+    # here, it is easy to run into numeric instability. Don't forget the        #
+    # regularization!                                                           #
+    #############################################################################
+    H = X @ W
+    XE = np.e ** H
+    true_indices = np.arange(y.shape[0]), y
+    good_XE = XE[true_indices]
+    XE_sums = XE.sum(axis=1)
+    loss = (-np.log(good_XE / XE_sums)).sum() / X.shape[0] + reg * np.sum(W ** 2)
 
-  dW /= X.shape[0]
-  dW += reg* 2 * W
+    grad_tmp = (X.T / XE_sums)
+    # print(grad_tmp.shape, XE.shape)
+    dW += (grad_tmp @ XE)
+    for i in range(W.shape[1]):
+        # print(X[np.argwhere(y == i)].sum(axis=0).squeeze().shape, dW.shape)
+        dW[:, i] -= X[np.argwhere(y == i)].sum(axis=0).squeeze()
 
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
-  
-  
-  return loss, dW
+    dW /= X.shape[0]
+    dW += reg * 2 * W
 
+    #############################################################################
+    #                          END OF YOUR CODE                                 #
+    #############################################################################
+
+    return loss, dW
